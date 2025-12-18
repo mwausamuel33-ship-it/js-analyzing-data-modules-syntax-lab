06130fe07681de281f69b49f38134214895f3ea2
samuel <mwausamuel33@gmail.com>
2025-12-18 12:02:02 +0300

    lab submission

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+//importing datejs.
+  var require=("datejs");
+
+
+function combineUsers(...args) {
+  //the return objects.
+  const combinedObject = {
+    users:[]
+  }
+
+  //looping all arrays and merging. 
+  args.forEach(array => {
+    combinedObject.users.push(...array)
+  })
+
+  //Add today's date in format M/d/yyyy.
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy')
+
+  //Return the final object.
+  return combinedObject
+}
 
 
 
